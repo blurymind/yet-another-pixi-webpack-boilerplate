93114776d78a7287ad3afb9e4212c5b1653a4a26
Todor Imreorov <blurymind@gmail.com>
2020-02-25 22:54:52 +0000

    Add experiments to the boilerplate - TODO: move to its own demo git repo

---
+++ src/js/tiled/heavy/TestBG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="TestBG" tilewidth="16" tileheight="16" tilecount="1140" columns="60">
+ <image source="old-dark-castle-interior-background.png" trans="000000" width="960" height="304"/>
+</tileset>
---
+++ src/js/tiled/heavy/TestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="TestTileset" tilewidth="16" tileheight="16" tilecount="315" columns="21">
+ <image source="gothic-castle-tileset.png" trans="000000" width="336" height="240"/>
+</tileset>
